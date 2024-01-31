eb6febd32d81be6c488c132cced4f227308af8de
Vikram9392 <vikramvallala12@gmail.com>
2024-01-31 10:18:36 +0530

    done

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.delete('/api/v1/details/:id',(req,res)=>{
 const id=req.params.id;
 const deletedUser = userDetails.find(
-  (updatedDetails) => updatedDetails.id == id
+  (updatedDetails) => updatedDetails.id === id
 );
 
 if(!deleteUser){
